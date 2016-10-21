cb7cdbd263cbd1568dc49a7c1a869bfef5e55a61
Jose Septien <jaseptienh@gmail.com>
2016-10-20 20:56:42 -0500

    Add implementation of connect() function.

--- tournament.py
+++ tournament.py
@@ -7,7 +7,7 @@ import psycopg2
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
-
+    return psycopg2.connect("dbname=tournament")
 
 def deleteMatches():
     """Remove all the match records from the database."""
